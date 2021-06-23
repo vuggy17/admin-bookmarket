e17a3477190dbfc91fcac2623ab19bfeeaaf843d
khangbdd <81902132+khangbdd@users.noreply.github.com>
2021-06-23 15:29:58 +0700

    config mvvm

---
+++ app/src/main/java/com/example/admin_bookmarket/CoreApplication.java
@@ -0,0 +1,9 @@
+package com.example.admin_bookmarket;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class CoreApplication extends Application {
+}

cf95b32a0066a8a5651a94aefb233c1bc88a98ea
khangbdd <81902132+khangbdd@users.noreply.github.com>
2021-06-26 17:21:01 +0700

    delete

--- app/src/main/java/com/example/admin_bookmarket/as.kt
+++
@@ -1,3 +0,0 @@
-package com.example.admin_bookmarket
-
-// TODO: 05/06/2021  lỗi khi sửa item xong rồi xoá
--- app/src/main/java/com/example/admin_bookmarket/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/admin_bookmarket/ui/home/HomeFragment.kt
@@ -2,22 +2,16 @@ package com.example.admin_bookmarket.ui.home
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.admin_bookmarket.ItemDetailActivity
 import com.example.admin_bookmarket.RecyclerViewClickListener
 import com.example.admin_bookmarket.SearchActivity
-import com.example.admin_bookmarket.SharedViewModel
 import com.example.admin_bookmarket.data.common.Constants.ITEM
 import com.example.admin_bookmarket.data.common.MarginItemDecoration
 import com.example.admin_bookmarket.data.model.Book

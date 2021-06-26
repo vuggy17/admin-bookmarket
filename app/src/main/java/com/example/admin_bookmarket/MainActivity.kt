f60fb828767974ed4194f18faf2f3a2939bf2b11
Phạm Minh Tân <tan.lk16.cla@gmail.com>
2021-06-26 21:08:21 +0700

    eror

--- app/src/main/java/com/example/admin_bookmarket/MainActivity.kt
+++ app/src/main/java/com/example/admin_bookmarket/MainActivity.kt
@@ -15,15 +15,15 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.admin_bookmarket.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+
+
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private var isBackPressed = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,46 +31,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-//        binding.appBarMain.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val bottonNavActionView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//        val navController = findNavController(R.id.fragment)
+//        bottonNavActionView.setupWithNavController(navController)
 
     }
 
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        return super.onCreateView(name, context, attrs)
-        isBackPressed = false
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onBackPressed() {
-        val currentFragment =
-            findNavController(R.id.nav_host_fragment_content_main).currentDestination?.id
-        if (currentFragment == R.id.nav_home && isBackPressed) {
-            finish()
-        }
-        isBackPressed = true
-        Toast.makeText(applicationContext, "Press BACK again to exit", Toast.LENGTH_SHORT)
-            .show()
-        Handler().postDelayed(Runnable { isBackPressed = false }, 1000)
-        super.onBackPressed()
-    }
 }

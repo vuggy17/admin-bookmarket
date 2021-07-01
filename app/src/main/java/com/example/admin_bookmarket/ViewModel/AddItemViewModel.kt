12ec20404bc7f2fa2b1974e8be6918a66022ecbb
Phạm Minh Tân <tan.lk16.cla@gmail.com>
2021-07-02 03:38:15 +0700

    fix bug 1

--- app/src/main/java/com/example/admin_bookmarket/ViewModel/AddItemViewModel.kt
+++ app/src/main/java/com/example/admin_bookmarket/ViewModel/AddItemViewModel.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
+@HiltViewModel
 class AddItemViewModel @Inject constructor(
     @ApplicationContext private val appContext: Context
 ): ViewModel() {

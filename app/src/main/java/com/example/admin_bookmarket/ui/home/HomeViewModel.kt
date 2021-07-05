1206ad9a7a22c659235035ab3cdb4049e5ed5e2c
vuggy17 <vuggy17@gmail.com>
2021-07-05 23:08:49 +0700

    final fix

--- app/src/main/java/com/example/admin_bookmarket/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/admin_bookmarket/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.admin_bookmarket.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,6 +12,7 @@ import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : ViewModel() {
@@ -25,11 +27,29 @@ class HomeViewModel @Inject constructor() : ViewModel() {
     private fun loadBook()
     {
         FirebaseFirestore.getInstance().collection("books").addSnapshotListener(
-            object: EventListener<QuerySnapshot>{
+            object: EventListener<QuerySnapshot> {
                 override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
-                    lstCurrentBook.value = FullBookList.getInstance().lstFullBook
+                    if (error != null) {
+                        return
+                    }
+                    val bookList: MutableList<Book> = ArrayList()
+                    for (doc in value!!) {
+                        val bookItem = Book(
+                            doc.id,
+                            doc.data["Image"].toString(),
+                            doc.data["Name"].toString(),
+                            doc.data["Author"].toString(),
+                            doc.data["Price"].toString().toDouble().roundToInt(),
+                            doc.data["rate"].toString().toDouble().roundToInt(),
+                            doc.data["Kind"].toString(),
+                            doc.data["Counts"].toString().toDouble().roundToInt(),
+                            doc.data["ImageID"].toString(),
+                            doc.data["Description"].toString()
+                        )
+                        bookList.add(bookItem)
+                    }
+                    lstCurrentBook.value = bookList
                 }
-
             }
         )
     }
